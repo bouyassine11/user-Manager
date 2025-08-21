b891aa6fef8970352d8c206a23743a2512f35874
bouyassine11 <yassinebouyahia19@gmail.com>
2025-08-21 19:56:06 +0100

    add user

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -19,15 +19,40 @@ export const UserForm: React.FC<UserFormProps> = ({onAdd})=>{
         //" hello ".trim() → "hello" ✅ non-empty → passes.
         //" ".trim() → "" ❌ empty → won’t pass.
         e.preventDefault();
-        
+        if(name.trim() && email.trim() && password.trim()){
+            onAdd({
+                name: name.trim(),
+                email: email.trim(),
+                password: password.trim(),
+                type
+            });
+            //reste the form field
+            console.log("dooooooone")
+            setName('');
+            setEmail('');
+            setPassword('');
+            setType('user');
+        }
     }
 
 
 
 
     return(
-        <>
-        
-        </>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Add a user name" required/>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Add an email" required/>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="add a strong password" required/>
+                <select value={type} onChange={(e)=>setType(e.target.value as User['type'])}>
+                    <option value="user">User</option>
+                    <option value="admin">Admin</option>
+                    <option value="editor">Editor</option>
+                </select>
+            </div>
+            <button type="submit">
+                Add User
+            </button>
+        </form>
     )
 }
